dfd914b999b03abbc549e86449d81efa7ef71b47
Daniel Kiefer <danielfmpinho@gmail.com>
2023-01-26 11:52:44 -0300

    header index classIntoFunction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,31 @@
-import { Component } from 'react';
-import { FiPlusSquare } from 'react-icons/fi';
+import React from "react";
+import { FiPlusSquare } from "react-icons/fi";
 
-import { Container } from './styles';
-import Logo from '../../assets/logo.svg';
+import { Container } from "./styles";
+import Logo from "../../assets/logo.svg";
 
-class Header extends Component {
-  render() {
-    const { openModal } = this.props;
+interface Props {
+  openModal: () => void;
+}
 
-    return (
-      <Container>
-        <header>
-          <img src={Logo} alt="GoRestaurant" />
-          <nav>
-            <div>
-              <button
-                type="button"
-                onClick={openModal}
-              >
-                <div className="text">Novo Prato</div>
-                <div className="icon">
-                  <FiPlusSquare size={24} />
-                </div>
-              </button>
-            </div>
-          </nav>
-        </header>
-      </Container>
-    )
-  }
+const Header: React.FC<Props> = ({ openModal }) => {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button type="button" onClick={openModal}>
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  );
 };
 
 export default Header;
